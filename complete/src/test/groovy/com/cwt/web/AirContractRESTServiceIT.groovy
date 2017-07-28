df5f974597e0849036ac4990464be63f07f07a5b
Bezzerbt <kambiz.shahri@gmail.com>
2017-07-28 16:37:32 +0200

    Done the PointOfSaleScope business rule.

--- complete/src/test/groovy/com/cwt/web/AirContractRESTServiceIT.groovy
+++ complete/src/test/groovy/com/cwt/web/AirContractRESTServiceIT.groovy
@@ -24,16 +24,17 @@ class AirContractRESTServiceIT extends Specification {
     def slurper = new JsonSlurper()
 
     def setupSpec() {
+        //TODO::find out what this actually does...
         client.handler.failure = client.handler.success
     }
 
     @IgnoreRest
     @Unroll
     def "REST Contract creation with #headerInfoInput "(String payloadInput,
-                                                              HttpStatus expectedHttpStatusInput,
-                                                              int expectedFieldErrorCount,
-                                                              String expectedFieldValueInput,
-                                                              String expectedMessageValueInput, String headerInfoInput) {
+                                                        HttpStatus expectedHttpStatusInput,
+                                                        int expectedFieldErrorCount,
+                                                        String expectedFieldValueInput,
+                                                        String expectedMessageValueInput, String headerInfoInput) {
         when:
         Map payloadForJBossInstance = slurper.parseText(payloadInput)
         Map actualRequest = [:]
@@ -45,46 +46,70 @@ class AirContractRESTServiceIT extends Specification {
         def resp = client.post(actualRequest)
 
         then:
-        //Replacing the following with a method call
         assert responseVersusExpectedResolver(resp, expectedHttpStatusInput,
                 expectedFieldErrorCount, expectedFieldValueInput, expectedMessageValueInput)
 
         where:
         payloadInput | expectedHttpStatusInput | expectedFieldErrorCount | expectedFieldValueInput | expectedMessageValueInput | headerInfoInput
-        '{"contractScope":{"description":"MyContract","systemName":"systematic","currencyCode":"EUR","owner":"Alberto Palma","validityStartDate":"2016-10-06","validityEndDate":"2017-10-05","typeCode":"SLA","statusCode":"Draft","mainSupplier":{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"},"dateTypeCode":"Booking","pointOfSaleScopeCode":"MultiCountry","pointsOfSale":[{"name":"","code":"AB"}],"active":true,"validatingSuppliers":[{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"}],"marketingSuppliers":[],"operatingSuppliers":[],"businessDivisionCodes":["Commercial"],"contractLevel":"TICKET","remark":"hello"},"targets":[{"name":"target1","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","safetyNets":["safetyfirst"],"remarks":"remarksText","periodicity":"Monthly","payments":[{"name":"my Linear Payment","remarks":"linear"},{"name":"my Linear Payment","remarks":"not so linear"}]}],"safetyNets":[{"name":"safetyfirst","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","remarks":"remarksText"}]}' | HttpStatus.BAD_REQUEST | 1 | "contractScope.pointsOfSale" | "Must have either 0 or >1 point of sale(s) for MultiCountry pointOfScaleScope" | "MultiCountry PointOfSaleScope and 1 PointOfSale fails."
-        '{"contractScope":{"description":"MyContract","systemName":"systematic","currencyCode":"EUR","owner":"Alberto Palma","validityStartDate":"2016-10-06","validityEndDate":"2017-10-05","typeCode":"SLA","statusCode":"Draft","mainSupplier":{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"},"dateTypeCode":"Booking","pointOfSaleScopeCode":"International","pointsOfSale":[{"name":"","code":"AB"}],"active":true,"validatingSuppliers":[{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"}],"marketingSuppliers":[],"operatingSuppliers":[],"businessDivisionCodes":["Commercial"],"contractLevel":"TICKET","remark":"hello"},"targets":[{"name":"target1","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","safetyNets":["safetyfirst"],"remarks":"remarksText","periodicity":"Monthly","payments":[{"name":"my Linear Payment","remarks":"linear"},{"name":"my Linear Payment","remarks":"not so linear"}]}],"safetyNets":[{"name":"safetyfirst","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","remarks":"remarksText"}]}' | HttpStatus.BAD_REQUEST | 1 | "contractScope.pointsOfSale" | "Must have either 0 or >1 point of sale(s) for International pointOfScaleScope" | "International PointOfSaleScope and 1 PointOfSale fails."
+        '{"contractScope":{"description":"MyContract","systemName":"systematic","currencyCode":"EUR","owner":"Alberto Palma","validityStartDate":"2016-10-06","validityEndDate":"2017-10-05","typeCode":"SLA","statusCode":"Draft","mainSupplier":{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"},"dateTypeCode":"Booking","pointOfSaleScopeCode":"","pointsOfSale":[{"name":"","code":"AB"}],"active":true,"validatingSuppliers":[{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"}],"marketingSuppliers":[],"operatingSuppliers":[],"businessDivisionCodes":["Commercial"],"contractLevel":"TICKET","remark":"hello"},"targets":[{"name":"target1","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","safetyNets":["safetyfirst"],"remarks":"remarksText","periodicity":"Monthly","payments":[{"name":"my Linear Payment","remarks":"linear"},{"name":"my Linear Payment","remarks":"not so linear"}]}],"safetyNets":[{"name":"safetyfirst","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","remarks":"remarksText"}]}' | HttpStatus.BAD_REQUEST | 1 | "contractScope.pointOfSaleScopeCode." | "Invalid PointOfSaleScope" | "no PointOfSaleScope provided FAILS!!"
+        '{"contractScope":{"description":"MyContract","systemName":"systematic","currencyCode":"EUR","owner":"Alberto Palma","validityStartDate":"2016-10-06","validityEndDate":"2017-10-05","typeCode":"SLA","statusCode":"Draft","mainSupplier":{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"},"dateTypeCode":"Booking","pointOfSaleScopeCode":"MultiCountry","pointsOfSale":[{"name":"","code":"AB"}],"active":true,"validatingSuppliers":[{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"}],"marketingSuppliers":[],"operatingSuppliers":[],"businessDivisionCodes":["Commercial"],"contractLevel":"TICKET","remark":"hello"},"targets":[{"name":"target1","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","safetyNets":["safetyfirst"],"remarks":"remarksText","periodicity":"Monthly","payments":[{"name":"my Linear Payment","remarks":"linear"},{"name":"my Linear Payment","remarks":"not so linear"}]}],"safetyNets":[{"name":"safetyfirst","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","remarks":"remarksText"}]}' | HttpStatus.BAD_REQUEST | 1 | "contractScope.pointsOfSale" | "Must have either 0 or >1 point of sale(s) for MultiCountry pointOfScaleScope" | "MultiCountry PointOfSaleScope and 1 PointOfSale FAILS!!"
+        '{"contractScope":{"description":"MyContract","systemName":"systematic","currencyCode":"EUR","owner":"Alberto Palma","validityStartDate":"2016-10-06","validityEndDate":"2017-10-05","typeCode":"SLA","statusCode":"Draft","mainSupplier":{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"},"dateTypeCode":"Booking","pointOfSaleScopeCode":"International","pointsOfSale":[{"name":"","code":"AB"}],"active":true,"validatingSuppliers":[{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"}],"marketingSuppliers":[],"operatingSuppliers":[],"businessDivisionCodes":["Commercial"],"contractLevel":"TICKET","remark":"hello"},"targets":[{"name":"target1","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","safetyNets":["safetyfirst"],"remarks":"remarksText","periodicity":"Monthly","payments":[{"name":"my Linear Payment","remarks":"linear"},{"name":"my Linear Payment","remarks":"not so linear"}]}],"safetyNets":[{"name":"safetyfirst","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","remarks":"remarksText"}]}' | HttpStatus.BAD_REQUEST | 1 | "contractScope.pointsOfSale" | "Must have either 0 or >1 point of sale(s) for International pointOfScaleScope" | "International PointOfSaleScope and 1 PointOfSale FAILS!!."
+        '{"contractScope":{"description":"MyContract","systemName":"systematic","currencyCode":"EUR","owner":"Alberto Palma","validityStartDate":"2016-10-06","validityEndDate":"2017-10-05","typeCode":"SLA","statusCode":"Draft","mainSupplier":{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"},"dateTypeCode":"Booking","pointOfSaleScopeCode":"International","pointsOfSale":[{"name":"","code":"AB"}, {"name":"","code":"TZ"}],"active":true,"validatingSuppliers":[{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"}],"marketingSuppliers":[],"operatingSuppliers":[],"businessDivisionCodes":["Commercial"],"contractLevel":"TICKET","remark":"hello"},"targets":[{"name":"target1","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","safetyNets":["safetyfirst"],"remarks":"remarksText","periodicity":"Monthly","payments":[{"name":"my Linear Payment","remarks":"linear"},{"name":"my Linear Payment","remarks":"not so linear"}]}],"safetyNets":[{"name":"safetyfirst","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","remarks":"remarksText"}]}' | HttpStatus.OK | 0 | "" | "" | "International PointOfSaleScope >1 PointOfSale SUCCEEDS!!."
+        '{"contractScope":{"description":"MyContract","systemName":"systematic","currencyCode":"EUR","owner":"Alberto Palma","validityStartDate":"2016-10-06","validityEndDate":"2017-10-05","typeCode":"SLA","statusCode":"Draft","mainSupplier":{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"},"dateTypeCode":"Booking","pointOfSaleScopeCode":"International","pointsOfSale":[],"active":true,"validatingSuppliers":[{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"}],"marketingSuppliers":[],"operatingSuppliers":[],"businessDivisionCodes":["Commercial"],"contractLevel":"TICKET","remark":"hello"},"targets":[{"name":"target1","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","safetyNets":["safetyfirst"],"remarks":"remarksText","periodicity":"Monthly","payments":[{"name":"my Linear Payment","remarks":"linear"},{"name":"my Linear Payment","remarks":"not so linear"}]}],"safetyNets":[{"name":"safetyfirst","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","remarks":"remarksText"}]}' | HttpStatus.OK | 0 | "" | "" | "International PointOfSaleScope 0 PointOfSale SUCCEEDS!!."
+        '{"contractScope":{"description":"MyContract","systemName":"systematic","currencyCode":"EUR","owner":"Alberto Palma","validityStartDate":"2016-10-06","validityEndDate":"2017-10-05","typeCode":"SLA","statusCode":"Draft","mainSupplier":{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"},"dateTypeCode":"Booking","pointOfSaleScopeCode":"MultiCountry","pointsOfSale":[{"name":"","code":"AB"}, {"name":"","code":"SC"}],"active":true,"validatingSuppliers":[{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"}],"marketingSuppliers":[],"operatingSuppliers":[],"businessDivisionCodes":["Commercial"],"contractLevel":"TICKET","remark":"hello"},"targets":[{"name":"target1","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","safetyNets":["safetyfirst"],"remarks":"remarksText","periodicity":"Monthly","payments":[{"name":"my Linear Payment","remarks":"linear"},{"name":"my Linear Payment","remarks":"not so linear"}]}],"safetyNets":[{"name":"safetyfirst","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","remarks":"remarksText"}]}' | HttpStatus.OK | 0 | "" | "" | "MultiCountry PointOfSaleScope >1 PointOfSale SUCCEEDS!!."
+        '{"contractScope":{"description":"MyContract","systemName":"systematic","currencyCode":"EUR","owner":"Alberto Palma","validityStartDate":"2016-10-06","validityEndDate":"2017-10-05","typeCode":"SLA","statusCode":"Draft","mainSupplier":{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"},"dateTypeCode":"Booking","pointOfSaleScopeCode":"MultiCountry","pointsOfSale":[],"active":true,"validatingSuppliers":[{"name":"CHINA SOUTHERN AIRLINES","code":"CZ"}],"marketingSuppliers":[],"operatingSuppliers":[],"businessDivisionCodes":["Commercial"],"contractLevel":"TICKET","remark":"hello"},"targets":[{"name":"target1","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","safetyNets":["safetyfirst"],"remarks":"remarksText","periodicity":"Monthly","payments":[{"name":"my Linear Payment","remarks":"linear"},{"name":"my Linear Payment","remarks":"not so linear"}]}],"safetyNets":[{"name":"safetyfirst","agreementTypeCode":"Performance Based","revenueTypeCode":"Service Fee","remarks":"remarksText"}]}' | HttpStatus.OK | 0 | "" | "" | "MultiCountry PointOfSaleScope 0 PointOfSale SUCCEEDS!!."
     }
 
-    boolean responseVersusExpectedResolver(Object resp,
+    /**
+     * This utility method simply makes sure that the conditions for the fixture instance
+     * are met in order to indicate if it has succeeded or failed.
+     * It should itself have a unit test in order to make sure it is sound.
+     * @param resp is the RESTClient return object after the REST call
+     * @param expectedHttpStatus the expected status of this instance of the REST call
+     * @param expectedFieldErrorCount the expected number of validation errors
+     * @param expectedFieldValueInput the expected field name for an error
+     * @param expectedMessageValueInput the expected field error message for an error
+     * @return boolean with true == REST call succeeded and false for a failure
+     */
+    private boolean responseVersusExpectedResolver(Object resp,
                                            HttpStatus expectedHttpStatus,
                                            int expectedFieldErrorCount,
                                            String expectedFieldValueInput,
                                            String expectedMessageValueInput) {
 
-        try {
-            assert "Assert HttpStatus was:" + expectedHttpStatus.value() + "", resp.status == expectedHttpStatus.value()
-            int fieldErrorCount = 0
-            try {
-                fieldErrorCount = resp.data.entrySet().size()
-            } catch (Exception e) {
-                //carry on
-            }
-            assert fieldErrorCount == expectedFieldErrorCount
-            if (expectedFieldErrorCount == 0) return true
 
-            //Now we look at the expected error fields (overkill or what?)
-            assert resp.data.entrySet()[0].getProperties().get("key").equals("fieldErrors")
-            List<Map> fieldErrorList = resp.data.entrySet()[0].getProperties().get("value")
-            assert fieldErrorList != null
-
-            for (item in fieldErrorList) {
-                println "FieldErrorEntry was: ${item}"
-                item.get("field").equals(expectedFieldValueInput)
-                item.get("message").equals(expectedMessageValueInput)
-            }
-        }
-        catch (AssertionError e) {
+        if (!expectedHttpStatus.value().equals(resp.status)) {
             return false
+        }
+        int actualFieldErrorCount = 0
+        try {
+            actualFieldErrorCount = resp.data.entrySet()[0].getProperties().get("key").equals("fieldErrors")
+        } catch (Exception e) {
+            //carry on
+        }
+        if (!expectedFieldErrorCount == actualFieldErrorCount) {
+            return false
+        }
+        if (expectedFieldErrorCount == 0) {
+            return true
+        }
+        List<Map> fieldErrorList = null
+        try {
+            resp.data.entrySet()[0].getProperties().get("key").equals("fieldErrors")
+            fieldErrorList = resp.data.entrySet()[0].getProperties().get("value")
+        } catch (Exception e1) {
+            return false
+        }
+        if (fieldErrorList == null) {
+            return false
+        }
+        for (item in fieldErrorList) {
+            if (!item.get("field").equals(expectedFieldValueInput)) {
+                return false
+            }
+            if (!item.get("message").equals(expectedMessageValueInput)) {
+                return false;
+            }
         }
         return true;
     }
